c6c5523028842b0d879ed80414e83f210d8dd0a9
aleanrocha <aleanrocha.dev@gmail.com>
2024-07-08 16:43:11 -0300

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 
 export const App = () => {
   return (
     <>
       <Header />
-      <main>Hello</main>
+      <main className="bg-todo-white min-h-[80vh] py-8 px-4 flex justify-center items-center">
+        Hello
+      </main>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-primary-color text-todo-white py-12 px-4 text-center">
+      <p className="text-lg">
+        @2024 - Desenvolvido por
+        <a
+          href="https://linkedin.com/in/aleanrocha"
+          className="text-secondary-color font-bold"
+        >
+          {' '}
+          Alean Rocha
+        </a>
+      </p>
+    </footer>
+  )
+}
